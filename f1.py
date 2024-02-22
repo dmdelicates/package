2121fbfaa29db9ac2a480958c15ba88edb6bd1dd
dmdelicates <124428948+dmdelicates@users.noreply.github.com>
2024-02-22 16:49:40 +0500

    Update f1.py

--- f1.py
+++ f1.py
@@ -7,9 +7,3 @@ def gen_list():
     return l
 def cur_d():
     return datetime.date(datetime.today())
-
-def low(l1):
-    l2 = []
-    for l in l1:
-        l2.append(datetime.isoweekday(l))
-    return l2
